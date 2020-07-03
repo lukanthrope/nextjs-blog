d625033a7ba159e59b35fd8f95dd4651d7b7caf0
lukanthrope <bydlocoder228@gmail.com>
2020-07-04 02:02:02 +0300

    bit of refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,8 @@
-import App, { AppInitialProps, AppContext } from 'next/app';
+import App, { AppInitialProps } from 'next/app';
 import { wrapper } from '../redux/store';
 import { RootDiv } from '../styles';
 
 class WrappedApp extends App<AppInitialProps> {
-  public static getInitialProps = async ({ Component, ctx }: AppContext) => {
-    return {
-      pageProps: {
-        ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
-        appProp: ctx.pathname,
-      },
-    };
-  };
-
   public render() {
     const { Component, pageProps } = this.props;
     return (
